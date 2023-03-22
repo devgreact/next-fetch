3427643cbe0e1612bbb065cb4fff558d91bba775
greact <dev.greact@gmail.com>
2023-03-22 11:58:55 +0900

    1차 연동 작업

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React, { Children } from "react";
+import HeaderInfo from "./HeaderInfo";
+import Nav from "./Nav";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <HeaderInfo />
+      <Nav />
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default Layout;
